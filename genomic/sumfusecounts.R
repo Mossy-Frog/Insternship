ed44571278d0baab0c354c41bdc72772426322cc
Maxence LECHEMIA <86064030+Mossy-Frog@users.noreply.github.com>
2023-08-07 14:31:21 +0200

    Update sumfusecounts.R

--- genomic/sumfusecounts.R
+++ genomic/sumfusecounts.R
@@ -1,14 +1,14 @@
-chemin<-"C:/Users/maxence.lechemia/Documents/Génomique/Genomiccounts formated2/"
+chemin<-"./"
 list_files <- list.files(path =chemin,full.names = T)
 print(list_files)
-for (i in list_files)
+for (file in list_files)
 {
-  print(i)
-  list_files_into <- list.files(path =i,full.names = T)
+  print(file)
+  list_files_into <- list.files(path =file,full.names = T)
   table1<-read.table(list_files_into[1])
   table2<-read.table(list_files_into[2])
   tablefinal<-table1
   tablefinal$V2<-table1$V2+table2$V2
-  write.table(tablefinal,paste0(i,".txt"), sep = "  ", dec = ".",
+  write.table(tablefinal,paste0(file,".txt"), sep = "  ", dec = ".",
             row.names = F, col.names = F)
 }
